2fd9c3f11f0aa1e8baca63b59db2424f43f23a2b
arranzizquierdo <laura.arranz88@gmail.com>
2018-12-17 16:22:30 +0100

    Create html and js field

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,44 @@
 'use strict';
 
-console.log('>> Ready :)');
+const buttonEl = document.querySelector('.button__search');
+const inputEl = document.querySelector('.input__name');
+const listResultEl = document.querySelector('.list__result');
+
+
+function handlePrintResult() {
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+
+        let imgFilm;
+        let nameFilm;
+        let itemFilm;
+        let containerFilm;
+        
+        for (let i = 0; i < data.length; i++) {
+
+                containerFilm = document.createElement('div');
+                containerFilm.classList.add('container__film');
+
+                itemFilm = document.createElement('li');
+                itemFilm.classList.add('item__list');
+
+                imgFilm = document.createElement('img');
+                imgFilm.src = data[i].show.image.medium;
+
+                nameFilm = document.createElement('h2');
+                const textName = document.createTextNode(data[i].show.name);
+                nameFilm.appendChild(textName);
+
+                itemFilm.appendChild(imgFilm);
+                itemFilm.appendChild(nameFilm);
+
+                containerFilm.appendChild(itemFilm);
+
+                listResultEl.appendChild(containerFilm);
+            }  
+})
+}
+
+buttonEl.addEventListener('click', handlePrintResult);
