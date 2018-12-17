cce463d710b04fc421d4c2193cf04a51536c5c9d
arranzizquierdo <laura.arranz88@gmail.com>
2018-12-17 17:23:36 +0100

    Create js to print list result and add class favorite when click

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -6,26 +6,30 @@ const listResultEl = document.querySelector('.list__result');
 
 
 function handlePrintResult() {
-  fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
+    fetch(`http://api.tvmaze.com/search/shows?q=${inputEl.value}`)
+        .then(response => response.json())
+        .then(data => {
 
-        let imgFilm;
-        let nameFilm;
-        let itemFilm;
-        let containerFilm;
-        
-        for (let i = 0; i < data.length; i++) {
+            let imgFilm;
+            let nameFilm;
+            let itemFilm;
+            // let containerFilm;
 
-                containerFilm = document.createElement('div');
-                containerFilm.classList.add('container__film');
+            for (let i = 0; i < data.length; i++) {
+
+                // containerFilm = document.createElement('div');
+                // containerFilm.classList.add('container__film');
 
                 itemFilm = document.createElement('li');
                 itemFilm.classList.add('item__list');
 
                 imgFilm = document.createElement('img');
-                imgFilm.src = data[i].show.image.medium;
+                if (data[i].show.image === null) {
+                    imgFilm.src = "https://via.placeholder.com/210x295/cccccc/666666/?text=TV";
+                } else {
+                    let srcImage = data[i].show.image.medium;
+                    imgFilm.src = srcImage;
+                };
 
                 nameFilm = document.createElement('h2');
                 const textName = document.createTextNode(data[i].show.name);
@@ -34,11 +38,22 @@ function handlePrintResult() {
                 itemFilm.appendChild(imgFilm);
                 itemFilm.appendChild(nameFilm);
 
-                containerFilm.appendChild(itemFilm);
+                // containerFilm.appendChild(itemFilm);
 
-                listResultEl.appendChild(containerFilm);
-            }  
-})
+                listResultEl.appendChild(itemFilm);
+            }
+        })
+}
+
+function handleClickFavorite (event) {
+    const click = event.target;
+    const parentElement = click.parentElement;
+    parentElement.classList.toggle('favorite');
+    const arrFavorites = document.querySelectorAll('.favorite');
+    console.log(arrFavorites);
+    localStorage.setItem('favorites', JSON.stringify(arrFavorites));
 }
 
 buttonEl.addEventListener('click', handlePrintResult);
+listResultEl.addEventListener('click', handleClickFavorite);
+
